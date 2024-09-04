46ffdf8ca65002ced900da6d8792ad888fcaa8b0
Cai <2217228878@qq.com>
2024-09-04 10:23:13 +0800

    commit

---
+++ setting.py
@@ -0,0 +1,14 @@
+opengauss_config = {
+    'database': '',  # 选择数据库名称
+    'user': '',  # 连接数据库的用户名
+    'password': '',  # 连接数据库的用户密码
+    'host': '',  # 数据库ip
+    'port': '',  # 数据库服务器监听的端口号
+    'client_encoding': 'UTF-8'
+}
+
+SparkApi_config = {
+    'appid': '',
+    'apikey': '',
+    'apisecret': '',
+}
